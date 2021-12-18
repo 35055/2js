cc4b46cde3c12b1b065988243c9ae9a111b48d4f
Murat <bxndbdbvsj@gmail.com>
2021-12-18 18:52:24 +0300

    content

---
+++ js/third.js
@@ -0,0 +1,81 @@
+var x = +prompt('Введите время')
+switch(x){
+    case 0:
+        alert('0 часов ночь')
+        break
+    case 1:
+        alert('1 час ночи')
+        break
+    case 2:
+        alert('2 часа ночи')
+        break
+        case 3:
+            alert('3 часа ночи')
+            break
+        case 4:
+            alert('4 часа ночи')
+            break
+            case 5:
+                alert('5 часов утра')
+                break
+            case 6:
+                alert('6 часов утра')
+                break
+                case 7:
+                    alert('7 часов утра')
+                    break
+                case 8:
+                    alert('8 часов утра')
+                    break
+                    case 9:
+                        alert('9 часов утра')
+                        break
+                    case 10:
+                        alert('10 часов утра')
+                        break
+                        case 11:
+                            alert('11 часов утра')
+                            break
+                        case 12:
+                            alert('12 часов полдень')
+                            break
+                            case 13:
+                                alert('13 часов обед')
+                                break
+                            case 14:
+                                alert('14 часов обед')
+                                break
+                                case 15:
+                                    alert('15 часов обед')
+                                    break
+                                case 16:
+                                    alert('16 часов обед')
+                                    break
+                                    case 17:
+                                        alert('17 часов вечера')
+                                        break
+                                    case 18:
+                                        alert('18 часов вечера')
+                                        break
+                                        case 19:
+                                            alert('19 часов вечера')
+                                            break
+                                        case 20:
+                                            alert('20 часов вечера')
+                                            break
+                                            case 21:
+                                                alert('21 часов ночь')
+                                                break
+                                            case 22:
+                                                alert('22 часов ночь')
+                                                break
+                                                case 23:
+                                                    alert('23 часов ночь')
+                                                    break
+                                                case 24:
+                                                    alert('0 часов ночь')
+                                                    break
+                                                    default:
+                                                        alert('Вы ввели неправильно')
+                                                        break
+}
